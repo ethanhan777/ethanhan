f41f25ce8948dfa6751e4f84ee5cf18505964714
Ethan Han <ehan@penguinrandomhouse.com>
2019-02-24 16:45:55 -0500

    updated homepage styling.

--- src/app/molecules/mlcl_nav/mlcl-nav.component.ts
+++ src/app/molecules/mlcl_nav/mlcl-nav.component.ts
@@ -9,10 +9,15 @@ import {
   styleUrls: ['./mlcl-nav.component.scss']
 })
 export class MlclNavComponent implements OnInit {
+  menuActive = false;
 
   constructor() {}
 
   ngOnInit() {
     // this.subscriptions.add();
+  }
+
+  menuToggle() {
+    this.menuActive = !this.menuActive;
   }
 }
